65e5f179e0860e9c264ca5437ee29ed0b73412f4
lianakoleva <lkoleva@imsa.edu>
2020-02-29 20:30:04 -0600

    here ya go gabey

--- backend/backend.py
+++ backend/backend.py
@@ -1,30 +1,52 @@
-from concurrent import futures
-from __future__ import print_function
-import logging
-import grpc
+import datapackage
+import pandas as pd
+import pandas.series as sr
 
-# for reading from database
-import filein_pb2
-import filein_pb2_grpc
+iso_url = 'https://datahub.io/core/country-codes/datapackage.json'
 
-# for writing to gateway
-import fileout_pb2
-import fileout_pb2_grpc
+# to load data package into storage
+package = datapackage.Package(iso_url)
 
-# get data from database
-def run():
-    
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = filein_pb2_grpc.databaseStub(channel)
-        response = stub.readAll(filein_pb2.read(ready='yes'))
-    # add response to dataframe
-
-# give data to gateway
-class out ():
-    
-    def temps (self, request, context):
-        return fileout_pb2.temp_pt()
-    
-    def geos (self, request, context):
-        return fileout_pb2.geo_pt()
-    
+# to load only tabular data
+resources = package.resources
+for resource in resources:
+    if resource.tabular:
+        iso = pd.read_csv(resource.descriptor['path'])
+        corona = pd.read_csv('02-29-2020.csv')
+        iso = iso.loc[:,['official_name_en','ISO3166-1-numeric']]
+        corona = corona.merge(right='iso', how='left', 
+                              right_on='official_name_en', 
+                              left_on='Country/Region')
+        sr
+        
+#
+#from concurrent import futures
+#from __future__ import print_function
+#import logging
+#import grpc
+#
+## for reading from database
+#import filein_pb2
+#import filein_pb2_grpc
+#
+## for writing to gateway
+#import fileout_pb2
+#import fileout_pb2_grpc
+#
+## get data from database
+#def run():
+#    
+#    with grpc.insecure_channel('localhost:50051') as channel:
+#        stub = filein_pb2_grpc.databaseStub(channel)
+#        response = stub.readAll(filein_pb2.read(ready='yes'))
+#    # add response to dataframe
+#
+## give data to gateway
+#class out ():
+#    
+#    def temps (self, request, context):
+#        return fileout_pb2.temp_pt()
+#    
+#    def geos (self, request, context):
+#        return fileout_pb2.geo_pt()
+#    
